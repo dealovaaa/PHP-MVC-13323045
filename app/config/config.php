1e37f20486278b626334ef4a839fce5bfc7a7365
dealovaaa <151107631+dealovaaa@users.noreply.github.com>
2024-04-11 00:24:27 +0700

    Create config.php

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    define('BASEURL','http://localhost/phpmvc/PHP-MVC-13323045/public');
+
+    //DB
+    define ('DB_HOST','localhost');
+    define ('DB_USER','root');
+    define ('DB_PASS','');
+    define('DB_NAME','phpmvc');
